49a4768109d1ebcd3b454a1f2095374d44b7fc2f
dhouha123 <dhouha123@users.noreply.github.com>
2021-08-05 17:03:37 +0200

    Update app/src/main/java/com/dimelo/sampleapp/MainActivity.java
    Co-authored-by: Driss Tahraoui-Maldague <driss.tahraouimaldague@gmail.com>

--- app/src/main/java/com/dimelo/sampleapp/MainActivity.java
+++ app/src/main/java/com/dimelo/sampleapp/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-
         // Setup Dimelo
         final Dimelo dimelo = RcConfig.setupDimelo(this);
         dimelo.setDimeloListener(dimeloListener);
@@ -113,7 +112,6 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
 }
-
 
 
 
