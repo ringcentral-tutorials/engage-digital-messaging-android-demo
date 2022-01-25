71ccd8b295e9e22e668edb0c6c5f66fe9a0c53f7
Dhouha Chikhaoui <dhouha.chikhaoui@ringcentral.com>
2022-01-25 10:20:03 +0100

    clean code

--- app/src/main/java/com/dimelo/sampleapp/chats/TabTelecom.java
+++ app/src/main/java/com/dimelo/sampleapp/chats/TabTelecom.java
@@ -9,8 +9,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
